86199d0e7d2f533ecd609767cec0e6f198a92a57
TsvetozaraTosheva <ttosheva@lt.life>
2022-05-09 00:07:55 +0300

    Cleaning master branch

--- src/test/java/ReverseString.java
+++
@@ -1,8 +0,0 @@
-public class ReverseString {
-    public static void main(String[] args) {
-        String str = "Мыло";
-        StringBuffer sbr = new StringBuffer(str);
-        sbr.reverse();
-        System.out.println(sbr);
-    }
-}
